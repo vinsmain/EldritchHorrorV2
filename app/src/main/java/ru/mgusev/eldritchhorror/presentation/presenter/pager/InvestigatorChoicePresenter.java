88e6c58a7ebe5687efc9840aa2e011c3228ba445
mGusev <vinsmain@gmail.com>
2018-05-25 21:00:10 +0500

    1.8.1 Выбор и удаление сыщиков

--- app/src/main/java/ru/mgusev/eldritchhorror/presentation/presenter/pager/InvestigatorChoicePresenter.java
+++ app/src/main/java/ru/mgusev/eldritchhorror/presentation/presenter/pager/InvestigatorChoicePresenter.java
@@ -4,6 +4,7 @@ import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -21,6 +22,8 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
     @Inject
     Repository repository;
     private List<Investigator> investigatorList;
+    private List<Investigator> activeInvestigatorList;
+    private List<Investigator> allInvestigatorList;
     private List<Expansion> expansionList;
     private CompositeDisposable investigatorSubscribe;
     private CompositeDisposable expansionSubscribe;
@@ -29,6 +32,7 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
         App.getComponent().inject(this);
         expansionSubscribe = new CompositeDisposable();
         expansionSubscribe.add(repository.getExpansionPublish().subscribe(this::updateInvListByExpansion));
+        activeInvestigatorList = new ArrayList<>();
         investigatorList = repository.getInvestigatorList();
         updateInvListByExpansion(repository.getExpansionList());
         investigatorSubscribe = new CompositeDisposable();
@@ -43,7 +47,7 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
     private void updateInvListByExpansion(List<Expansion> list) {
         if (!list.equals(expansionList)) {
             expansionList = list;
-            List<Investigator> tempList = new ArrayList<>();
+            /*List<Investigator> tempList = new ArrayList<>();
             List<Investigator> allList = repository.getInvestigatorList();
 
             for (Investigator inv : investigatorList) {
@@ -55,6 +59,12 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
                     tempList.add(investigator);
             }
             investigatorList = tempList;
+            getViewState().updateAllItems(investigatorList);*/
+
+            updateAllInv();
+            investigatorList.clear();
+            investigatorList.addAll(activeInvestigatorList);
+            investigatorList.addAll(allInvestigatorList);
             getViewState().updateAllItems(investigatorList);
         }
     }
@@ -75,7 +85,7 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
         //investigatorList.set(investigatorList.indexOf(currentInvestigator), currentInvestigator);
         //updateInvListByExpansion(expansionList);
 
-        int position = 0;
+        /*int position = 0;
         for (int i = 0; i < investigatorList.size(); i++) {
             if (investigatorList.get(i).getId() == currentInvestigator.getId()) {
                 position = i;
@@ -89,8 +99,63 @@ public class InvestigatorChoicePresenter extends MvpPresenter<InvestigatorChoice
         } else {
             investigatorList.remove(position);
             getViewState().removeItem(position, investigatorList);
+        }*/
+
+        int position = 0;
+        if (currentInvestigator.isStarting() || currentInvestigator.isReplacement()) {
+            if (activeInvestigatorList.contains(currentInvestigator)) {
+                position = activeInvestigatorList.indexOf(currentInvestigator);
+                activeInvestigatorList.remove(currentInvestigator);
+                activeInvestigatorList.add(0, currentInvestigator);
+            } else {
+                position = allInvestigatorList.indexOf(currentInvestigator) + activeInvestigatorList.size();
+                activeInvestigatorList.add(0, currentInvestigator);
+            }
+            getViewState().removeItem(position, investigatorList);
+
+            //getViewState().updateItem(position, investigatorList);
+        } else {
+            if (activeInvestigatorList.contains(currentInvestigator)) {
+                position = activeInvestigatorList.indexOf(currentInvestigator);
+                activeInvestigatorList.remove(currentInvestigator);
+                
+                activeInvestigatorList.add(0, currentInvestigator);
+            } else {
+                position = allInvestigatorList.indexOf(currentInvestigator) + activeInvestigatorList.size();
+                activeInvestigatorList.add(0, currentInvestigator);
+            }
+            getViewState().removeItem(position, investigatorList);
+
+
+
+
+            investigatorList.remove(position);
+            getViewState().removeItem(position, investigatorList);
         }
     }
+
+
+
+    private void updateAllInv() {
+        allInvestigatorList = repository.getInvestigatorList();
+        for (int i = 0; i < activeInvestigatorList.size(); i++) {
+            int index = allInvestigatorList.indexOf(activeInvestigatorList.get(i));
+            allInvestigatorList.remove(index);
+        }
+        allInvestigatorList = deleteDisableInv(allInvestigatorList);
+    }
+
+    private List<Investigator> deleteDisableInv(List<Investigator> list) {
+        List<Investigator> tempList = new ArrayList<>();
+        for (Investigator investigator : list) {
+            if (isActiveInv(investigator)) tempList.add(investigator);
+        }
+        return tempList;
+    }
+
+
+
+
 
     public void itemClick(int position) {
         repository.setInvestigator(investigatorList.get(position));
